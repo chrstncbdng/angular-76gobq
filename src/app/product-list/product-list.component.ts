823a6d6fccf9d32f2d6b19a331b18e9b510ea441
chrstncbdng <chrstncbdng@gmail.com>
2019-11-28 13:53:07 +0800

    3 files modified, 6 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,8 +10,12 @@ import { products } from '../products';
 export class ProductListComponent {
   products = products;
 
-  share() {
+  shareProduct() {
     window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale!!!');    
   }
 }
 
